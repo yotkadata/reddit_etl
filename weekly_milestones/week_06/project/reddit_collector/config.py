323c0da91a6cbac07fc3dabf32fba658dc83478b
Jan Kühn <yotka@posteo.org>
2023-05-06 11:12:22 +0200

    Add wordcloud script

--- weekly_milestones/week_06/project/reddit_collector/config.py
+++
@@ -1,7 +0,0 @@
-conf = {
-    "client_id": "",  # "Personal use script in Reddit"
-    "secret": "",  # "Secret" in Reddit
-    "username": "",
-    "password": "",
-    "headers": {"User-Agent": "TestAppforDocker"},
-}
